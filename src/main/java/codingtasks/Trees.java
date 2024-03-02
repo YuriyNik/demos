c2e3da783fbb435bf879789e629ef3047ef43deb
YuriyNik <yuriy.nikitenko@gmail.com>
2024-03-02 18:38:10 +0200

    more problems solved

---
+++ src/main/java/codingtasks/Trees.java
@@ -0,0 +1,52 @@
+package codingtasks;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Trees {
+    /**
+     * Definition for a binary tree node.
+     */
+    class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+    return false;
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+        int last = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()){
+            int count = q.size();
+            for (int i = 0; i<count;i++){
+                TreeNode curr = q.poll();
+                if (i==0)
+                    last = curr.val;
+                if (curr.left!=null)
+                    q.add(curr.left);
+                if (curr.right!=null)
+                    q.add(curr.right);
+            }
+        }
+        return last;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
