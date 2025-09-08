064d4b97af190a95e83cab723ff995c18f213c43
YuriyNik <yuriy.nikitenko@gmail.com>
2025-09-08 10:38:22 +0300

    solved string task 1

--- src/main/java/dryrun/StringTasks.java
+++ src/main/java/dryrun/StringTasks.java
@@ -2,6 +2,13 @@ package dryrun;
 
 public class StringTasks {
     public String firstEvenLengthWord(String s){
-        return null;
+        String[] words = s.split("\\s");
+        for (String word:words){
+            if (word.length()%2==0)
+            {
+                return word;
+            }
+        }
+        return "";
     }
 }
