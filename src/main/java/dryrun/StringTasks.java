c31545e25508755e2ff76e7586f5320525d8bd35
YuriyNik <yuriy.nikitenko@gmail.com>
2025-09-08 11:17:29 +0300

    solved task1-2 with streams

--- src/main/java/dryrun/StringTasks.java
+++ src/main/java/dryrun/StringTasks.java
@@ -1,26 +1,30 @@
 package dryrun;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class StringTasks {
     public String firstEvenLengthWord(String s){
         String[] words = s.split("\\s");
-        for (String word:words){
-            if (word.length()%2==0)
-            {
-                return word;
-            }
-        }
-        return "";
+//        for (String word:words){
+//            if (word.length()%2==0)
+//            {
+//                return word;
+//            }
+//        }
+//        return "";
+        return Arrays.stream(words).filter(w -> w.length()%2==0 ).findFirst().orElse("");
     }
     public String longestWord(String s){
         String[] words = s.split("\\s");
-        String longest = "";
-        for (String word:words){
-            if (word.length()>longest.length())
-            {
-                longest=word;
-            }
-        }
+//        String longest = "";
+//        for (String word:words){
+//            if (word.length()>longest.length())
+//            {
+//                longest=word;
+//            }
+//        }
 
-        return longest;
+        return Arrays.stream(words).max(Comparator.comparingInt(String::length)).orElse("");
     }
 }
